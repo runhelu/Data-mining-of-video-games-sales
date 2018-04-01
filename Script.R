2bf9a9694215e77f2526261182ecda82767ecf04
leolurunhe <leolu1996@outlook.com>
2018-03-31 22:28:46 -0400

    Add files via upload

--- Script.R
+++ Script.R
@@ -55,6 +55,32 @@ all_2016 = data[data$Year_of_Release == "2016",]
 all_2017 = data[data$Year_of_Release == "2017",]
 year = c(1996:2017)
 global_sale_year = c(sum(all_1996$Global_Sales), sum(all_1997$Global_Sales), sum(all_1998$Global_Sales), sum(all_1999$Global_Sales), sum(all_2000$Global_Sales), sum(all_2001$Global_Sales), sum(all_2002$Global_Sales), sum(all_2003$Global_Sales), sum(all_2004$Global_Sales), sum(all_2005$Global_Sales), sum(all_2006$Global_Sales), sum(all_2007$Global_Sales), sum(all_2008$Global_Sales), sum(all_2009$Global_Sales), sum(all_2010$Global_Sales), sum(all_2011$Global_Sales), sum(all_2012$Global_Sales), sum(all_2013$Global_Sales), sum(all_2014$Global_Sales), sum(all_2015$Global_Sales), sum(all_2016$Global_Sales), sum(all_2017$Global_Sales))
-plot(year,global_sale_year)
-best_1996=all_1996[which(all_1996$Global_Sales == max(all_1996$Global_Sales)),]
-best_1997=all_1997[which(all_1997$Global_Sales == max(all_1997$Global_Sales)),]
+plot(year, global_sale_year, type = 'b', lwd = 2,main="Global sales vs. Year")
+
+#in order to predict sales:
+# 80% training data, 20% test data
+set.seed(12345)
+trainid = sample(1:nrow(data), size = trunc(nrow(data) * 0.8))
+trainData = data[trainid,]
+testData = data[-trainid,]
+#Predict sale
+# first basic method: lasso regression
+library(glmnet)
+set.seed(12345)
+trainx = model.matrix(Global_Sales ~ . - NA_Sales - EU_Sales - JP_Sales - Other_Sales - Global_Sales - Name-Publisher-Year_of_Release, data = trainData)[, -1]
+trainy = trainData$Global_Sales
+testx = model.matrix(Global_Sales ~ . - NA_Sales - EU_Sales - JP_Sales - Other_Sales - Global_Sales - Name-Publisher-Year_of_Release, data = testData)[, -1]
+testy = testData$Global_Sales
+#use cross validation to choose the best lambda in lasso regression
+cv.out = cv.glmnet(trainx, trainy, alpha = 1)
+bestlambda = cv.out$lambda.min
+
+#using the best lambda to do lasso regression:
+model.lasso = glmnet(trainx, trainy, lambda = bestlambda,alpha = 1)
+lasso_train_pred = predict(model.lasso, s = bestlambda, newx = trainx)
+lasso_train_mse = mean((lasso_train_pred - trainy) ^ 2)
+lasso_test_pred = predict(model.lasso, s = bestlambda, newx = testx)
+lasso_test_mse = mean((lasso_test_pred - testy) ^ 2)
+lasso_train_mse
+lasso_test_mse
+lasso_coef = predict(model.lasso, s = bestlambda, type = "coefficient")[1:200,]
